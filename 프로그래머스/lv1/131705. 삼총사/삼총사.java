d3de75a21d68c662162c69bbc23afabaad7faa4b
shuroop2 <kingdho3o@gmail.com>
2022-10-24 00:55:46 +0900

    Update 삼총사.java

--- 프로그래머스/lv1/131705. 삼총사/삼총사.java
+++ 프로그래머스/lv1/131705. 삼총사/삼총사.java
@@ -3,15 +3,9 @@ class Solution {
         int answer = 0;
 
         for(int i=0; i<number.length; i++) {
-            int num1 = number[i];
-            
             for(int j=i+1; j<number.length; j++) {
-                int num2 = number[j];
-                
                 for(int k=j+1; k<number.length; k++) {
-                    int num3 = number[k];
-                    
-                    if(num1 + num2 + num3 == 0)
+                    if(number[i] + number[j] + number[k] == 0)
                         answer++;
                 }
             }
