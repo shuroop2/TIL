373280bf32f7ae4e5fc6a489fee4e4b75641e01d
shuroop2 <kingdho3o@gmail.com>
2022-10-31 23:38:41 +0900

    [level 1] Title: 신규 아이디 추천, Time: 1.47 ms, Memory: 74.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/72410. 신규 아이디 추천/신규 아이디 추천.java
@@ -0,0 +1,22 @@
+class Solution {
+    public String solution(String new_id) {
+        String answer = "";
+        answer = new_id.toLowerCase();
+        answer = answer.replaceAll("[^-_.a-z0-9]", "");
+        answer = answer.replaceAll("[.]{2,}", ".");
+        answer = answer.replaceAll("^[.]|[.]$", "");
+        
+        if(answer.length() == 0)
+            answer += "a";
+        if(answer.length() >= 16) {
+            answer = answer.substring(0, 15);
+            answer = answer.replaceAll("^[.]|[.]$", "");
+        }
+        if(answer.length() < 3) {
+            while(answer.length() < 3) {
+                answer += answer.charAt(answer.length() - 1);
+            }
+        }
+        return answer;
+    }
+}
