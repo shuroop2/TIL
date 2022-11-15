6b8ffd9997f48c774f5788fc40f779235dc2a5dd
shuroop2 <kingdho3o@gmail.com>
2022-11-15 19:41:17 +0900

    [level 2] Title: 전화번호 목록, Time: 230.59 ms, Memory: 97.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Solution {
+    public boolean solution(String[] phone_book) {
+        boolean result = true;
+        Arrays.sort(phone_book);
+
+        for(int i=0; i<phone_book.length-1; i++) {
+            if(phone_book[i+1].startsWith(phone_book[i]))
+                result = false;
+        }
+       return result;
+    }
+}
