7b7320ce66f54199ed0dea13f03ef7eda840344a
shuroop2 <kingdho3o@gmail.com>
2022-11-23 16:20:48 +0900

    [unrated] Title: 푸드 파이트 대회, Time: 2.14 ms, Memory: 80.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String solution(int[] food) {
+        String answer = "";
+        
+        for(int i=1; i<food.length; i++) {
+            for(int j=0; j<food[i]/2; j++)
+                answer += Integer.toString(i);
+        }
+        
+        answer += "0";
+        
+        for(int i=answer.length()-2; i>=0; i--)
+            answer += answer.substring(i, i+1);
+
+        return answer;
+    }
+}
