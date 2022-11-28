dec0814974c538977a6da5ffd3bdf59a1a2865dc
shuroop2 <kingdho3o@gmail.com>
2022-11-28 19:16:12 +0900

    [unrated] Title: 귤 고르기, Time: 45.42 ms, Memory: 91.3 MB -BaekjoonHub

--- 프로그래머스/unrated/138476. 귤 고르기/귤 고르기.java
+++ 프로그래머스/unrated/138476. 귤 고르기/귤 고르기.java
@@ -11,13 +11,11 @@ class Solution {
         ArrayList<Integer> keySet = new ArrayList<>(map.keySet());
         Collections.sort(keySet, (o1, o2) -> (map.get(o2).compareTo(map.get(o1))));
         
-        while(k > 0) {
-            for(int i : keySet) {
-                k -= map.get(i);
-                answer++;
-                if(k <= 0)
-                    break;
-            }
+        for(int i : keySet) {
+            k -= map.get(i);
+            answer++;
+            if(k <= 0)
+                break;
         }
         
         return answer;
