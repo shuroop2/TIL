1890e3a24e3bdf70aa0384181d1cb5d2432e328c
shuroop2 <kingdho3o@gmail.com>
2023-03-02 21:57:47 +0900

    [Silver V] Title: 단어 정렬, Time: 548 ms, Memory: 24316 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.java
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -1,28 +1,25 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.Arrays;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
-		List<String> list = new ArrayList<>();
+		String[] arr = new String[n];
 		
-		for(int i=0; i<n; i++) {
-			String tmp = br.readLine();
-			if(!list.contains(tmp))
-				list.add(tmp);
-		}
+		for(int i=0; i<n; i++)
+			arr[i] = br.readLine();
 		
-		Collections.sort(list, (o1, o2) -> {
+		Arrays.sort(arr, (o1, o2) -> {
 			return o1.length() == o2.length() ? o1.compareTo(o2) : o1.length() - o2.length();
 		});
 		
-		for(String s : list)
-			System.out.println(s);
+		System.out.println(arr[0]);
+		for(int i=1; i<n; i++)
+			if(!arr[i].equals(arr[i-1]))
+				System.out.println(arr[i]);
 		
 		br.close();
 	}
