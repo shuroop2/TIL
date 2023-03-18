2b79b1bfee1f1ae641990160b398b3f73c6f780d
shuroop2 <kingdho3o@gmail.com>
2023-03-18 17:54:34 +0900

    [Silver IV] Title: 대칭 차집합, Time: 832 ms, Memory: 83840 KB -BaekjoonHub

---
+++ 백준/Silver/1269. 대칭 차집합/대칭 차집합.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
+		Set<Integer> setA = new HashSet<>();
+		Set<Integer> setB = new HashSet<>();
+		int answer = 0;
+		
+		st = new StringTokenizer(br.readLine());
+		for(int i=0; i<a; i++)
+			setA.add(Integer.parseInt(st.nextToken()));
+		
+		st = new StringTokenizer(br.readLine());
+		for(int i=0; i<b; i++)
+			setB.add(Integer.parseInt(st.nextToken()));
+		
+		for(int i : setA)
+			if(!setB.contains(i))
+				answer++;
+		
+		for(int i : setB)
+			if(!setA.contains(i))
+				answer++;
+		
+		System.out.println(answer);
+			
+		br.close();
+	}
+}
