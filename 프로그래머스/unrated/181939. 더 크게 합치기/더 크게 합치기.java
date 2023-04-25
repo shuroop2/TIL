3ebcbb58e9d871bc6a0ab20474100a3d030abc19
shuroop2 <kingdho3o@gmail.com>
2023-04-25 20:15:36 +0900

    [unrated] Title: 더 크게 합치기, Time: 1.34 ms, Memory: 75.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181939. 더 크게 합치기/더 크게 합치기.java
@@ -0,0 +1,5 @@
+class Solution {
+    public int solution(int a, int b) {
+        return Math.max(Integer.parseInt(String.valueOf(b) + String.valueOf(a)), Integer.parseInt(String.valueOf(a) + String.valueOf(b)));
+    }
+}
