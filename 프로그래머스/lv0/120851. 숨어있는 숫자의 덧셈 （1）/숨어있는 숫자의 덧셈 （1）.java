ce530e2209190db9444fd28fbeab2d6cb3ab4c00
shuroop2 <kingdho3o@gmail.com>
2023-06-03 22:55:51 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (1), Time: 0.10 ms, Memory: 74.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int solution(String my_string) {
+        int answer = 0;
+        my_string = my_string.replaceAll("\\D", "");
+        for(int i=0; i<my_string.length(); i++)
+            answer += my_string.charAt(i) - '0';
+        return answer;
+    }
+}
