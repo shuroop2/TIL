62b7134cea1b6c9d92106cc656b9cfe310e2c7bf
shuroop2 <kingdho3o@gmail.com>
2023-06-04 14:18:55 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.02 ms, Memory: 78.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int[] solution(int n) {
+        int[] answer = new int[(n + 1) / 2];
+        for(int i=1; i<=n; i+=2)
+            answer[i / 2] = i;
+        return answer;
+    }
+}
